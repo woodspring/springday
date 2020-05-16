f6e28a044173436ec45fd29ffc6d407060ef28f0
woodspring <woodspring.toronto@gmail.com>
2020-05-16 16:06:41 -0400

    dd Mongo

--- src/main/java/woodspring/springday/constant/MongoConst.java
+++ src/main/java/woodspring/springday/constant/MongoConst.java
@@ -3,6 +3,7 @@ package woodspring.springday.constant;
 public class MongoConst {
 	public static final String MongoDbName = "Woodspring";
 	public static final String MongoDbHostIpName = "192.168.0.119";
+	public static final String MongoDbReactive = "Woodspringreactive";
 	
 
 }
