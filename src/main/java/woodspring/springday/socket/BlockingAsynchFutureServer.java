0777ed370ba6737101ed5d8371a4a77a79d3fa0b
nancy <nancy@localhost>
2022-07-24 11:15:54 -0400

    import service

--- src/main/java/woodspring/springday/socket/BlockingAsynchFutureServer.java
+++ src/main/java/woodspring/springday/socket/BlockingAsynchFutureServer.java
@@ -2,6 +2,7 @@ package woodspring.springday.socket;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 @Service("BlockingAsynchFutureServer")
 public class BlockingAsynchFutureServer {
